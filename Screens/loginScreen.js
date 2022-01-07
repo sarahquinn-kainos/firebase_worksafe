93906c2b3c50c56b2f75f4671360eb55019237d7
Sarah Quinn <32102318+squinnie@users.noreply.github.com>
2022-01-07 16:28:24 +0000

    account type routing for admin and standard users

--- Screens/loginScreen.js
+++ Screens/loginScreen.js
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from 'react'
-import {auth} from '../firebase'
+import React, { useState, useEffect } from 'react'
+import { auth } from '../firebase'
+import { firestore } from '../firebase'
 import { useNavigation } from '@react-navigation/core'
 import { StyleSheet, Text, View } from 'react-native'
 import { KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
@@ -7,94 +8,108 @@ import { TouchableNativeFeedback } from 'react-native-web'
 
 const loginScreen = () => {
 
+    // get data from firestore 
+    const getUserAccountType = async () => {
+        const user = auth.currentUser;
+        const snapshot = await firestore
+            .collection('Users').doc(user.uid).get();
+        const myDoc = snapshot.data()
+        const account_type = myDoc.account_type
+        return account_type;
+    }
+
+
     // const for storing user input fields
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-  
+
     const navigation = useNavigation()
     const forgotPassword = () => {
         navigation.navigate("ForgotPassword");
     }
-  
+
     // change page after login 
     useEffect(() => {
-      const unsubscribe = auth.onAuthStateChanged(user => {
-        if (user) {
-            if (user.uid == "vJ1wPthO22eyMQ6iu8ukcZVwQWB3"){ // replace with role value later
-                navigation.replace("AdminDash")
-            }else{
-                navigation.replace("Home")
+        const unsubscribe = auth.onAuthStateChanged(async user => {
+            if (user) {
+
+                const account_type = await getUserAccountType();
+                console.log("\n\n===LOG===\n ACCOUNT TYPE ADMIN: " + account_type)
+                if (account_type == "super_user" || account_type == "admin") { // replace with role value later
+                    navigation.replace("AdminDash")
+                } else {
+                    navigation.replace("Home")
+                }
             }
-        }
-      })
-  
-      return unsubscribe
+        })
+
+        return unsubscribe
     }, [])
-  
+
     // create a new user in firebase 
     const handleSignUp = () => {
         navigation.replace("Register")
-    //   auth
-    //     .createUserWithEmailAndPassword(email, password)
-    //     .then(userCredentials => {
-    //       const user = userCredentials.user;
-    //       console.log('Registered with:', user.email);
-    //     })
-    //     .catch(error => alert(error.message))
+        //   auth
+        //     .createUserWithEmailAndPassword(email, password)
+        //     .then(userCredentials => {
+        //       const user = userCredentials.user;
+        //       console.log('Registered with:', user.email);
+        //     })
+        //     .catch(error => alert(error.message))
     }
-  
+
 
     // login with firebase auth 
     const handleLogin = () => {
-      auth
-        .signInWithEmailAndPassword(email, password)
-        .then(userCredentials => {
-          const user = userCredentials.user;
-          console.log('Logged in with:', user.email);
-          console.log('User ID:', user.uid);
-        })
-        .catch(error => alert(error.message))
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(userCredentials => {
+                const user = userCredentials.user;
+                console.log('Logged in with:', user.email);
+                console.log('User ID:', user.uid);
+            })
+            .catch(error => alert(error.message))
     }
 
     return (
         //Input fields &  Buttons
         <KeyboardAvoidingView
-        style={styles.container}
-        behaviour = "padding">
-        <View
-         style={styles.inputContainer}
-         behaviour = "padding">
-             <TextInput 
-             placeholder="Email"
-             value={email}
-             onChangeText={text => setEmail(text)}
-             style = {styles.input}>
-             </TextInput>
+            style={styles.container}
+            behaviour="padding">
+            <View
+                style={styles.inputContainer}
+                behaviour="padding">
+                <TextInput
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={text => setEmail(text)}
+                    style={styles.input}>
+                </TextInput>
 
-             <TextInput 
-             placeholder="Password"
-             value={password}
-             onChangeText={text => setPassword(text)}
-             style = {styles.input}
-             secureTextEntry>
-             </TextInput>
+                <TextInput
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={text => setPassword(text)}
+                    style={styles.input}
+                    secureTextEntry>
+                </TextInput>
 
-         </View>
-         <View 
-         style ={styles.buttonContainer}>
-             <TouchableOpacity
-             onPress = {handleLogin}
-             style = {styles.button}>
-                 <Text style = {styles.buttonText}>Login</Text>
-             </TouchableOpacity>
-             <TouchableOpacity
-             onPress = {handleSignUp}
-             style = {[styles.button, styles.buttonOutline]}>
-                 <Text style = {styles.buttonOutlineText}>Register</Text>
-             </TouchableOpacity>
-             <Text onPress = {forgotPassword} 
-             style={{ paddingTop: 50, justifyContent: 'center', alignItems: 'center' }}>Forgot Password</Text>
-         </View>
+            </View>
+            <View
+                style={styles.buttonContainer}>
+                <TouchableOpacity
+                    onPress={handleLogin}
+                    style={styles.button}>
+                    <Text style={styles.buttonText}>Login</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    onPress={handleSignUp}
+                    style={[styles.button, styles.buttonOutline]}>
+                    <Text style={styles.buttonOutlineText}>Register</Text>
+                </TouchableOpacity>
+                <Text onPress={forgotPassword}
+                    style={{ paddingTop: 50, justifyContent: 'center', alignItems: 'center' }}>Forgot Password</Text>
+            </View>
         </KeyboardAvoidingView>
     )
 }
@@ -108,43 +123,43 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
 
-    inputContainer : {
+    inputContainer: {
         width: '80%',
     },
-    input : {
+    input: {
         backgroundColor: 'white',
         paddingHorizontal: 15,
         paddingVertical: 10,
         borderRadius: 10,
         marginTop: 10
     },
-    buttonContainer : {
-        width : '60%', 
-        justifyContent:'center',
+    buttonContainer: {
+        width: '60%',
+        justifyContent: 'center',
         alignItems: 'center',
-        marginTop : 40
+        marginTop: 40
     },
-    button : {
-        backgroundColor : '#2B9AF6',
-        width : '100%',
+    button: {
+        backgroundColor: '#2B9AF6',
+        width: '100%',
         padding: 15,
-        borderRadius : 10,
-        marginTop : 10,
-        alignItems : 'center',
+        borderRadius: 10,
+        marginTop: 10,
+        alignItems: 'center',
     },
-    buttonOutline : {
-        backgroundColor : 'white',
+    buttonOutline: {
+        backgroundColor: 'white',
         borderColor: '#2B9AF6',
-        borderWidth : 2,
+        borderWidth: 2,
     },
     buttonText: {
-        color : 'white',
-        fontWeight : '600',
-        fontSize : 14
+        color: 'white',
+        fontWeight: '600',
+        fontSize: 14
     },
-    buttonOutlineText : {
-        color : '#2B9AF6',
-        fontWeight : '600',
-        fontSize : 14
+    buttonOutlineText: {
+        color: '#2B9AF6',
+        fontWeight: '600',
+        fontSize: 14
     },
 })
