e6a135b1c75365c9227bfd8862a2fd26ff34e5ae
Sarah Quinn <32102318+squinnie@users.noreply.github.com>
2022-01-13 23:12:02 +0000

    add function imports to admin page

--- Screens/adminDashboard.js
+++ Screens/adminDashboard.js
@@ -2,57 +2,36 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { auth } from '../firebase'
 import { firestore } from '../firebase'
+import { getSingleDocByDocId, getUsersCollection } from '../Javascript/firestore'
 
 const adminDashboard = () => {
     const user = auth.currentUser;
+    var currentUserDoc;
     if (user) {
         // current user's account vlaues 
         const email = user.email
         const displayName = user.displayName
 
-        // mapping for firestore object - returns a promise 
-        const collectIdsAndDocs = (doc) => {
-            return { id: doc.id, ...doc.data() };
-          };
-
-
-        // get data from firestore with a snapshot 
-        const getUserDocument = async () => {
-            const snapshot = await firestore
-            // WHOLE COLLECTION
-             //.collection('Users').get();
-            //const myDocs = snapshot.docs.map(collectIdsAndDocs);
-
-            //SINGLE DOCUMENT
-            .collection('Users').doc(user.uid).get();
-            const myDocs = snapshot.data()
-            console.log(myDocs);
-
-            //STORE DOC FIELD VALUES AS OBJECT - JSON
-            const docData = {
-                "email": myDocs.email,
-                "account_type": myDocs.account_type
-            }
-            return JSON.stringify(docData);
+        const getCurrentUserDoc = async () => {
+            currentUserDoc = await getSingleDocByDocId(user.uid).then(function (result) {
+                return result
+            })
         }
 
-        const getDocumentData = async () => {
-            const doc = await getUserDocument();
-            const docJSON = JSON.parse(doc)
-            const testFieldValue = docJSON.email
-            console.log("doc data: " + (testFieldValue))
+        getCurrentUserDoc().then(function () {
+            console.log(currentUserDoc)
+        })
 
-        }
-
-        getDocumentData();
-
+        getUsersCollection().then(function (result) {
+            console.log(result)
+        })
 
         return (
             <View>
                 <Text>Default Home Screen</Text>
                 <Text>{email}</Text>
                 <Text>{displayName}</Text>
-                
+
             </View>
         )
     }
