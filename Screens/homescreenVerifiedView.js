a8efe2a70396feaaa6971d84444187e77a7a8215
Sarah Quinn <32102318+squinnie@users.noreply.github.com>
2022-01-14 01:28:51 +0000

    extract data and push to UI

--- Screens/homescreenVerifiedView.js
+++ Screens/homescreenVerifiedView.js
@@ -1,26 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect, Component } from 'react'
 import { StyleSheet, Text, View, Image } from 'react-native'
 import { auth } from '../firebase'
 import { firestore } from '../firebase'
-import {getSingleDocByDocId, getUsersCollection} from '../Javascript/firestore'
+import { getSingleDocByDocId, getUsersCollection } from '../Javascript/firestore'
+
+
+
+// class User extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             currentUserDoc: {}
+//         }
+//     }
+// }
+
+
 
 const showVerifiedHome = () => {
-
+    const [currentUserDoc, setCurrentUserDoc] = useState(null);
+    const [email, setEmail] = useState('');
     const user = auth.currentUser;
-    if (user) {
-        getSingleDocByDocId(user.uid).then(function (result){
-            console.log(result)
-        })
-        getUsersCollection().then(function (result){
-            console.log(result)
-        })
-        return (
-            <View>
-                <Text>Default Home Screen</Text>
-                <Text>Your email address has been verified.</Text>
-            </View>
-        )
-    }
-}
 
+    if (!currentUserDoc) {
+        getSingleDocByDocId(user.uid).then(result => {
+            setCurrentUserDoc(result);
+            setEmail(result.email)}
+            // setCurrentUserDoc(result)}
+        );
+
+        //var currentUserDocJSON = JSON.parse(currentUserDoc);
+        //console.log()
+        //var myemail = currentUserDoc.email;
+    }
+    console.log(currentUserDoc)
+    return (
+        <View>
+            <Text>Default Home Screen</Text>
+            <Text>Your email address has been verified.</Text>
+
+            <Text>{email}</Text>
+        </View>
+    )
+
+}
 export default showVerifiedHome
