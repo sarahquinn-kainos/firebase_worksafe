3f523d583275c7ba882a4add2dfaa0d5f9e944cf
Sarah Quinn <32102318+squinnie@users.noreply.github.com>
2022-03-12 15:40:55 +0000

    working test cloud function - when checkpoint document added, a message is created in a notifications collection

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+    functions.logger.info("Hello logs!", { structuredData: true });
+    response.send("Hello from Firebase!");
 });
+
+const createTest = (message => {
+    return admin.firestore().collection('testNotification')
+    .add(message)
+    .then(doc => {
+        console.log('TEST notification added: ', doc)
+    })
+})
+
+
+exports.onCovidCheckpointCreated = functions.firestore
+    .document('CovidCheckpoints/{DocId}')
+    .onCreate(doc => {
+        const checkpointInstance = doc.data();
+        const message = {
+            content: "TEST content",
+            user: checkpointInstance.uid,
+            time: admin.firestore.FieldValue.serverTimestamp()
+        }
+        return createTest(message)
+    })
