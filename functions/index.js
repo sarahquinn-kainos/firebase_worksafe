525b71e525ecf9faf354e08314adcfefe942280f
Sarah Quinn <32102318+squinnie@users.noreply.github.com>
2022-03-14 21:13:07 +0000

    add alerts / warnings to affected shifts and create notification in schedule warning collection - NEED TO FIX VALIDATION

--- functions/index.js
+++ functions/index.js
@@ -42,13 +42,25 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 //     // }
 // });
 
-const addNotification = (message => {
-    return admin.firestore().collection('MailNotification')
+const addCovidNotification = (message => {
+    return admin.firestore().collection('CovidNotifications')
         .add(message)
         .then(doc => {
-            console.log('Mail Notification added: ', doc)
+            console.log('COVID Notification added: ', doc)
         })
 })
+
+const addScheduleWarning = (message => {
+    return admin.firestore().collection('ScheduleWarnings')
+        .add(message)
+        .then(doc => {
+            console.log('Schedule Warning added: ', doc)
+        })
+})
+
+const collectIdsAndDocs = (doc) => {
+    return { id: doc.id, ...doc.data() };
+};
 
 
 exports.onCovidCheckpointCreated = functions.firestore
@@ -68,52 +80,160 @@ exports.onCovidCheckpointCreated = functions.firestore
             const message = {
                 header: "COVID-19 WorkSafe Alert",
                 title: "Positive COVID-19 Checkpoint",
-                body: "Worker: " + checkpointInstance.user_display_name + ", has tested positive for COVID-19. Please open WorkSafe to view details and affected shifts.",
+                alert_message: "Worker: " + checkpointInstance.user_display_name + ", has tested positive for COVID-19.",
+                body: "Please open WorkSafe to view details and affected shifts.",
                 user: checkpointInstance.uid,
                 user_display_name: checkpointInstance.user_display_name,
                 time: timestamp,
                 alert_level: "high"
             }
             //alert is HIGH as user cannot work and shifts are affected 
-            return addNotification(message)
+            return addCovidNotification(message)
         }
         if (self_isolating) {
             const message = {
                 header: "COVID-19 WorkSafe Alert",
                 title: "Isolating - COVID-19 Checkpoint",
-                body: "Worker: " + checkpointInstance.user_display_name + ", is isolating from COVID-19. Please open WorkSafe to view details and affected shifts.",
+                alert_message: "Worker: " + checkpointInstance.user_display_name + ", is isolating from COVID-19.",
+                body: "Please open WorkSafe to view details and affected shifts.",
                 user: checkpointInstance.uid,
                 user_display_name: checkpointInstance.user_display_name,
                 time: timestamp,
                 alert_level: "high"
             }
             //alert is HIGH as user cannot work and shifts are affected 
-            return addNotification(message)
+            return addCovidNotification(message)
         }
         if (close_contact || symptoms) {
             const message = {
                 header: "COVID-19 WorkSafe Alert",
                 title: "Risk - COVID-19 Checkpoint",
-                body: "Worker: " + checkpointInstance.user_display_name + ", is a possible risk for COVID-19. Please open WorkSafe to details and affected shifts.",
+                alert_message: "Worker: " + checkpointInstance.user_display_name + ", is a possible risk for COVID-19.", 
+                body: "Please open WorkSafe to details and affected shifts.",
                 user: checkpointInstance.uid,
                 user_display_name: checkpointInstance.user_display_name,
                 time: timestamp,
                 alert_level: "medium"
             }
             //alert is MEDIUM as user may be OK to work - manual decision if shifts are affected by admin
-            return addNotification(message)
+            return addCovidNotification(message)
         }
         if (travelled) {
             const message = {
                 header: "COVID-19 WorkSafe Alert",
                 title: "Travel - COVID-19 Checkpoint",
-                body: "Worker: " + checkpointInstance.user_display_name + ", has recently travelled. Please open WorkSafe to view details and contact the worker for more information if required.",
+                alert_message: "Worker: " + checkpointInstance.user_display_name + ", has recently travelled.",
+                body: "Please open WorkSafe to view details and contact the worker for more information if required.",
                 user: checkpointInstance.uid,
                 user_display_name: checkpointInstance.user_display_name,
                 time: timestamp,
                 alert_level: "low"
             }
             //alert is LOW as majority of travel advice has been reduced - manual decision if shifts are affected by admin
-            return addNotification(message)
+            return addCovidNotification(message)
+        }
+    })
+
+exports.onCovidNotificationCreated = functions.firestore
+    .document('CovidNotifications/{DocId}')
+    .onCreate(async doc => {
+        var result;
+        var resultSnapshot;
+        const messageData = doc.data();
+        var alert_level = messageData.alert_level;
+        var alert_message = messageData.alert_message;
+        var affected_uid = messageData.user;
+        var start = messageData.time.toDate(); // 5 days before checkpoint
+        start.setDate(start.getDate() - 5)
+        var end = messageData.time.toDate(); // 5 days after checkpoint
+        end.setDate(start.getDate() + 5)
+        start.setUTCHours(0, 0, 0, 0);
+        end.setUTCHours(23, 59, 59, 999);
+        console.log("start: " + start)
+        console.log("end: " + end)
+        try {
+            // get data from firestore with a snapshot 
+            async function getDocument() {
+                const snapshot = await admin.firestore()
+                    .collection('WorkshiftSchedules')
+                    .where('date', '>=', start)
+                    .where('date', '<=', end)
+                    .where('staff_uids', 'array-contains', affected_uid)
+                    .get();
+                console.log(snapshot)
+                return snapshot
+            }
+            //get firestore data for affected shifts
+            resultSnapshot = await getDocument().then(function (response) {
+                return response
+            })
+            // if any shifts ARE affected:
+            if (!resultSnapshot.empty){
+                result = await getDocument().then(function (response) {
+                    mappedResponse = response.docs.map(collectIdsAndDocs);
+                    return mappedResponse
+                })
+                result.forEach((doc)=>{
+                    var message = {
+                        affected_user: affected_uid, // user whose checkpoint caused the notification
+                        shift_id: doc.id, // id of the shift
+                        alert_level: alert_level,
+                        shift_staff: doc.staff_uids
+                    }
+                    return addScheduleWarning(message)
+                })
+                // add warnng to the shift doc too 
+                resultSnapshot.forEach((doc) =>{
+                    //store all alert messages for shift in array
+                    var message_array = []
+                    message_array.push(alert_message) // add current alert message to array
+                    // avoid overwriting higher level alert!
+                    // if no existing alerts on shift - push new alert values 
+                    if (!(doc.hasOwnProperty("alert_level"))){
+                        doc.ref.update({
+                            "alert_level": alert_level,
+                            "alert_message": message_array
+                        })
+                    }else{    
+                         // get existing alert messages to append to array, if any exist
+                        if (doc.hasOwnProperty("alert_message")){
+                            doc.alert_message.forEach((message) => {
+                                message_array.push(message)
+                            })
+                        }
+                        // existing AND new alert level same value? No Comparison logic needed
+                        if (doc.alert_level == alert_level){
+                            doc.ref.update({
+                                "alert_level": alert_level,
+                                "alert_message":alert_message
+                            })
+                        }
+                        // existing alert more severe than new alert? keep existing value
+                        else if (
+                            (doc.alert_level == "high" && (alert_level == "medium" || alert_level == "low"))||
+                            (doc.alert_level == "medium" && alert_level == "low")
+                            ){
+                            doc.ref.update({
+                                "alert_level": doc.alert_level,
+                                "alert_message": message_array
+                            })
+                        }
+                        // if current alert lower than new alert - set value of new alert 
+                        else if (
+                            (doc.alert_level == "low" && (alert_level == "medium" || alert_level == "high"))||
+                            (doc.alert_level == "medium" && alert_level == "high")
+                            ){
+                            doc.ref.update({
+                                "alert_level": alert_level,
+                                "alert_message": message_array
+                            })
+                        }
+                    }
+                })
+            }   
+        }
+        catch (err) {
+            console.log(err)
+            response.status(500).send(err)
         }
     })
