ac4bb745988e1c7aac42c55870381cecc4027f8d
Sarah Quinn <32102318+squinnie@users.noreply.github.com>
2022-03-21 18:29:44 +0000

    working cloud functions to find impacts of covid positive result

--- functions/index.js
+++ functions/index.js
@@ -30,6 +30,14 @@ const collectIdsAndDocs = (doc) => {
     return { id: doc.id, ...doc.data() };
 };
 
+const addCloseContactNotification = (message) => {
+    return admin.firestore().collection('CloseContactNotifications')
+        .add(message)
+        .then(doc => {
+            console.log('Close Contact Warning added: ', doc)
+        })
+}
+
 
 exports.onCovidCheckpointCreated = functions.firestore
     .document('CovidCheckpoints/{DocId}')
@@ -76,7 +84,7 @@ exports.onCovidCheckpointCreated = functions.firestore
             const message = {
                 header: "COVID-19 WorkSafe Alert",
                 title: "Risk - COVID-19 Checkpoint",
-                alert_message: "Worker: " + checkpointInstance.user_display_name + ", is a possible risk for COVID-19.", 
+                alert_message: "Worker: " + checkpointInstance.user_display_name + ", is a possible risk for COVID-19.",
                 body: "Please open WorkSafe to details and affected shifts.",
                 user: checkpointInstance.uid,
                 user_display_name: checkpointInstance.user_display_name,
@@ -108,25 +116,34 @@ exports.onCovidNotificationCreated = functions.firestore
         var result;
         var resultSnapshot;
         const messageData = doc.data();
+        var notification_document;
+
+
         var alert_level = messageData.alert_level;
         var alert_message = messageData.alert_message;
-        var affected_uid = messageData.user;
+        var positive_user_uid = messageData.user;
+        var positive_user_display_name = messageData.user_display_name;
+        var checkpoint_date = messageData.time.toDate(); // checkpoint date
+        checkpoint_date.setUTCHours(0, 0, 0, 0);
         var start = messageData.time.toDate(); // 5 days before checkpoint
         start.setDate(start.getDate() - 5)
         var end = messageData.time.toDate(); // 5 days after checkpoint
-        end.setDate(start.getDate() + 5)
+        end.setDate(end.getDate() + 5)
         start.setUTCHours(0, 0, 0, 0);
         end.setUTCHours(23, 59, 59, 999);
         console.log("start: " + start)
         console.log("end: " + end)
+
+        // CREATE SCHEDULE NOTIFICATIONS IN FIRESTORE 'SCHEDULEWARNINGS' COLLECTION
+
         try {
-            // get data from firestore with a snapshot 
+            // get workshift data from firestore with a snapshot - 5 days either side of checkpoint 
             async function getDocument() {
                 const snapshot = await admin.firestore()
                     .collection('WorkshiftSchedules')
                     .where('date', '>=', start)
                     .where('date', '<=', end)
-                    .where('staff_uids', 'array-contains', affected_uid)
+                    .where('staff_uids', 'array-contains', positive_user_uid)
                     .get();
                 console.log(snapshot)
                 return snapshot
@@ -136,81 +153,113 @@ exports.onCovidNotificationCreated = functions.firestore
                 return response
             })
             // if any shifts ARE affected:
-            if (!resultSnapshot.empty){
+            if (!resultSnapshot.empty) {
                 result = await getDocument().then(function (response) {
                     mappedResponse = response.docs.map(collectIdsAndDocs);
                     return mappedResponse
                 })
-                result.forEach((doc)=>{
-                    var message = {
-                        affected_user: affected_uid, // user whose checkpoint caused the notification
-                        shift_id: doc.id, // id of the shift
-                        alert_level: alert_level,
-                        shift_staff: doc.staff_uids
-                    }
-                    return addScheduleWarning(message)
-                })
-                // add warnng to the shift doc too 
-                resultSnapshot.forEach((doc) =>{
-                    //const doc = document.data()
-                    //store all alert messages for shift in array
-                    var message_array = []
-                    message_array.push(alert_message) // add current alert message to array
+                var warning_notification_array = []
 
-                    // FIX THIS SECTION
-                    // avoid overwriting higher level alert!
-                    // if no existing alerts on shift - push new alert values 
-                    console.log('doc')
-                    console.log(doc)
-                    console.log('alert')
-                    console.log(alert)
-                    console.log('doc.alert_level')
-                    console.log(doc.alert_level)
-                    if (!('alert_level' in doc)){
-                        doc.ref.update({
-                            "alert_level": alert_level,
-                            "alert_message": message_array
-                        })
-                    }else{    
-                         // get existing alert messages to append to array, if any exist
-                        if ('alert_message' in doc){
-                            doc.alert_message.forEach((message) => {
-                                message_array.push(message)
-                            })
-                        }
-                        // existing AND new alert level same value? No Comparison logic needed
-                        if (doc.alert_level == alert_level){
-                            doc.ref.update({
-                                "alert_level": alert_level,
-                                "alert_message":alert_message
-                            })
-                        }
-                        // existing alert more severe than new alert? keep existing value
-                        else if (
-                            (doc.alert_level == "high" && (alert_level == "medium" || alert_level == "low"))||
-                            (doc.alert_level == "medium" && alert_level == "low")
-                            ){
-                            doc.ref.update({
-                                "alert_level": doc.alert_level,
-                                "alert_message": message_array
-                            })
-                        }
-                        // if current alert lower than new alert - set value of new alert 
-                        else if (
-                            (doc.alert_level == "low" && (alert_level == "medium" || alert_level == "high"))||
-                            (doc.alert_level == "medium" && alert_level == "high")
-                            ){
-                            doc.ref.update({
-                                "alert_level": alert_level,
-                                "alert_message": message_array
-                            })
-                        }
+                result.forEach((doc) => {
+                    var message = {
+                        positive_user_name: positive_user_display_name,
+                        positive_user_id: positive_user_uid, // user whose checkpoint caused the notification
+                        shift_id: doc.id, // id of the shift
+                        shift_date: doc.date,
+                        shift_start: doc.start_datetime,
+                        alert_level: alert_level, //from the covid notification document 
+                        alert_message: alert_message,
+                        shift_staff: doc.staff,
+                        staff_uids: doc.staff_uids
                     }
+                    // add warning to array
+                    warning_notification_array.push(message)
+
                 })
-            }   
+                // set doc data to write to collection 
+                notification_document = {
+                    shift_warnings: warning_notification_array,
+                }
+
+                //write array of warnings to the schedule warnings collection for notifications 
+                addScheduleWarning(notification_document)
+
+                // ADD ALERTS TO THE SHIFT DOCUMENTS IN FIRESTORE 'WORKSHIFTSCHEDULES' COLLECTION
+                resultSnapshot.forEach((document) => {
+                    const doc = document.data();
+                    var alert_data = {
+                        alert_level: alert_level,
+                        alert_message: alert_message
+                    }
+                    var alert_array = []
+                    const current_alert_array = doc.alerts;
+                    if (current_alert_array) { // append to existing alerts if any exist 
+                        alert_array = current_alert_array;
+                    }
+                    alert_array.push(alert_data)
+                    document.ref.update({
+                        "alerts": alert_array
+                    })
+                })
+            }
         }
         catch (err) {
             console.log(err)
             response.status(500).send(err)
+        }
+
+        if (messageData.title == "Positive COVID-19 Checkpoint"){
+            try {
+            // get workshift data from firestore with a snapshot - 5 days BEFORE checkpoint only
+            async function getDocumentPreviousDates() {
+                const snapshot = await admin.firestore()
+                    .collection('WorkshiftSchedules')
+                    .where('date', '>=', start)
+                    .where('date', '<=', checkpoint_date)
+                    .where('staff_uids', 'array-contains', positive_user_uid)
+                    .get();
+                console.log(snapshot)
+                return snapshot
+            }
+            //get firestore data for affected shifts
+            newResultSnapshot = await getDocumentPreviousDates().then(function (response) {
+                return response
+            })
+            // affected staff who have come into close contact on previous shifts (last 5 days) 
+            var affected_staff_array = []
+            newResultSnapshot.forEach((document) => {
+                const doc = document.data();
+                doc.staff_uids.forEach((id) => {
+                    if (id != positive_user_uid) { // avoid duplication of notification  
+                        affected_staff_array.push(id)
+                    }
+                })
+            })
+
+            function onlyUnique(value, index, self) {
+                return self.indexOf(value) === index;
+              }
+
+            // for every staff member in array - add notification to collection 
+            if (affected_staff_array.length > 0) {
+               
+                  var unique_ids = affected_staff_array.filter(onlyUnique);
+                  console.log("\n\nAffected staff (unique): " + unique_ids); 
+                  //let unique_ids = [new Set(affected_staff_array)]
+                  
+                  unique_ids.forEach((contact) => {
+                    var data = {
+                        staff_id: contact,
+                        positive_contact_id: positive_user_uid,
+                        date: checkpoint_date
+                    }
+                    addCloseContactNotification(data)
+                })
+            }
+        } catch (err) {
+            console.log(err)
+            response.status(500).send(err)
+        }
+
         }
     })
