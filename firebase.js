ceb9133cca079c85db9297d8cc62dbc5e0d74149
Sarah Quinn <32102318+squinnie@users.noreply.github.com>
2022-04-13 09:10:21 +0100

    env variables

--- firebase.js
+++ firebase.js
@@ -5,14 +5,15 @@ import "firebase/firestore"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD9jFkl1hlkiICQFyBX8hrQnRudTIxzY2w",
-  authDomain: "com667-7973b.firebaseapp.com",
-  projectId: "com667-7973b",
-  storageBucket: "com667-7973b.appspot.com",
-  messagingSenderId: "760562001764",
-  appId: "1:760562001764:web:2f040645bf3253ea06bc71",
-  measurementId: "G-LT03W4K01Q"
+  apiKey: env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
